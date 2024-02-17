58e1e78975a506cb464ea0709793c8871607591a
Tom brick <tombrickctf@gmail.com>
2024-02-17 10:39:46 +0530

    Update push.sh

--- automate/push.sh
+++ automate/push.sh
@@ -7,7 +7,7 @@ fi
 
 #prev token was of my personal acc so ignore it, m sry :(
 #this should work
-TOKEN="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"
+TOKEN="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"
 
 git add .
 git commit -m "$1"
